2b0a8822329e52bb5ee7423d665cbc033b43f625
natashawanja <kinuthianatasha@gmail.com>
2026-06-08 12:28:05 +0200

    feat: add all branch logins to login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-tsximport { useState } from 'react'
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 import api from '../api/axios'
